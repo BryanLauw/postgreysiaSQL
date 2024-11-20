2d3d250f95be5e707567bd7c66eca30ac7f44e8c
TazakiN <tazkia.nizami@gmail.com>
2024-11-20 13:46:12 +0700

    feat: looping input

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ from QueryProcessor.QueryProcessorClass import QueryProcessorClass
 
 queryProcessor = QueryProcessorClass()
 
-queryProcessor.execute_query("SELECT * FROM table1")
+print("\033[92mWelcome to PostgreysiaSQL!\033[0m")
+print(f"\033[94mYou can start using the SQL commands after the '>' prompt.\033[0m")
+
+while True:
+    query = input("> ")
+    if query == "exit":
+        break
+    queryProcessor.execute_query(query)
