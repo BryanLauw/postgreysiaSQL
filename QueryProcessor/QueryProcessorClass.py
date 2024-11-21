248366660338f0bfe277253f8229184a6a9e4d5f
Ben <sihombingbenjamin2004@gmail.com>
2024-11-21 14:37:07 +0700

    feat: add dummy printResult()

--- QueryProcessor/QueryProcessorClass.py
+++ QueryProcessor/QueryProcessorClass.py
@@ -7,6 +7,14 @@ class QueryProcessor:
         pass
 
     def execute_query(self, query : str):
+        tables = ["id", "name"]
+        rows = [
+            ['1', 'benfsjdjfdsfhuisdfosidfdjsopfjsduifbsdhyufdiogjdfbndifiodfuuiguirfhifhui'],
+            ['2', 'tazki'],
+            ['3', 'agil'],
+            ['4', 'filbert'],
+            ['5', 'bryan']
+        ]
         print("Executing query: " + query)
         if(query.upper() == "BEGIN" or query.upper() == "BEGIN TRANSACTION"):
             pass
@@ -14,5 +22,37 @@ class QueryProcessor:
             pass
         elif(query.upper() == "END TRANSACTION"):
             pass
+        elif(query.upper() == "PRINT"):
+            self.printResult(tables, rows)
         else:
             pass
+    
+    def ParsedQueryToDataRetrieval():
+        pass
+
+    def ParsedQueryToDataWrite():
+        pass
+
+    def ParsedQueryToDataDeletion():
+        pass
+
+    def printResult(self,column, data):
+        # Determine the maximum width of each column
+        column_widths = [max(len(row[i]) for row in data + [column]) for i in range(len(column))]
+
+        # Function to format a row
+        def format_row(row):
+            return "| " + " | ".join(row[i].ljust(column_widths[i]) for i in range(len(row))) + " |"
+
+        # Print the header
+        print("+-" + "-+-".join("-" * width for width in column_widths) + "-+")
+        print(format_row(column))
+        print("+-" + "-+-".join("-" * width for width in column_widths) + "-+")
+
+        # Print the data
+        for row in data:
+            print(format_row(row))
+
+        # Print the bottom border
+        print("+-" + "-+-".join("-" * width for width in column_widths) + "-+")
+
