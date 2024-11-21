48d6c464a6fc7858a1fe87e2b06f5cfa7382045d
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-11-22 02:35:22 +0700

    feat: add checking valid syntax with DFA (working)

--- Query Optimizer/QueryParser.py
+++ Query Optimizer/QueryParser.py
@@ -17,12 +17,13 @@ class QueryParser:
     def __init__(self, dfa_path: str):
         self.start_state = ""
         self.final_states = []
-        self.transitions = []
+        self.transitions = {}
         
         with open(dfa_path, "r") as file:
-            self.start_state = file.readline().strip()
+            self.start_state = file.readline().split()[1].strip()
             
-            self.final_states = file.readline().split().pop(0)            
+            self.final_states = file.readline().split()
+            self.final_states.pop(0)            
             
             while True:
                 parsed = file.readline().split()
@@ -30,9 +31,106 @@ class QueryParser:
                     break
                 
                 state = parsed[0]
+                self.transitions[state] = []
+                
                 lenParsed = len(parsed)
                 for i in range(1,lenParsed,2):
-                    print(i, parsed[i])
-                    self.transitions.append((state,parsed[i],parsed[i+1]))                
+                    self.transitions[state].append((parsed[i],parsed[i+1])) 
+    
+    def tokenize_query(self,query: str):
+        operators = self.CO + self.MO
+        
+        tokens = []
+        buffer = ""
+        i = 0
+
+        while i < len(query):
+            char = query[i]
+
+            # Whitespace
+            if char.isspace():
+                if buffer:
+                    tokens.append(buffer)
+                    buffer = ""
+                i += 1
+                continue
+            
+            # Symbols
+            if char in self.symbols:
+                if buffer:
+                    tokens.append(buffer)
+                    buffer = ""
+                tokens.append(char)
+                i += 1
+                continue
+
+            # Two char Operators (<= , <> , >=)
+            two_char_operator = query[i:i+2] 
+            if two_char_operator in operators:
+                if buffer:
+                    tokens.append(buffer)
+                    buffer = ""
+                tokens.append(two_char_operator)
+                i += 2
+                continue
+
+            # One char Operators
+            if char in ''.join(operators):
+                if buffer:
+                    tokens.append(buffer)
+                    buffer = ""
+                tokens.append(char)
+                i += 1
+                continue
+
+            # String
+            if char == '"':
+                if buffer:
+                    tokens.append(buffer)
+                    buffer = ""
+                end_quote = query.find('"', i + 1)
+                if end_quote == -1:
+                    raise ValueError("Unterminated string in query")
+                tokens.append(query[i:end_quote + 1])
+                i = end_quote + 1
+                continue
+
+            buffer += char
+            i += 1
+
+        if buffer:
+            tokens.append(buffer)
+
+        final_tokens = []
+        for token in tokens:
+            if token.upper() in self.keywords:
+                final_tokens.append(token.upper())
+            else:
+                final_tokens.append(token)
+
+        return final_tokens
+    
+    def check_valid_syntax(self,query: str):
+        tokens = self.tokenize_query(query)
+        
+        cur_state = self.start_state
+        for token in tokens:
+            next_state = ""
+            cur_state_rules = self.transitions[cur_state]
+            for rule in cur_state_rules:
+                rule_token = rule[0]
+                if((token == rule_token) or (rule_token == "<X>" and token.isalnum()) or
+                   (rule_token == "<N>" and token.isnumeric()) or (rule_token == "<CO>" and token in self.CO) or
+                   (rule_token == "<MO>" and token in self.MO)
+                ):
+                    next_state = rule[1]
+                    break
                 
-QueryParser("dfa.txt")
+            if not next_state:
+                return False
+            
+            cur_state = next_state
+        return cur_state in self.final_states
+        
+test = QueryParser("dfa.txt")
+print(test.check_valid_syntax("SELECT a,b,     c FROM b as b NATURAL JOIN c AS WHERE a>1"))          
