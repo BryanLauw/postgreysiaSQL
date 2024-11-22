c335b46952b17c9516cb98aa6b393df15c2c936c
HanifIHM <imamhanif2003@gmail.com>
2024-11-22 22:14:48 +0700

    feat : add cost by time

--- QueryOptimizer/QueryCost.py
+++ QueryOptimizer/QueryCost.py
@@ -28,3 +28,37 @@ class QueryCost:
     def select(self, table: str, attribute: str) -> int: # returns number of tuples
         statistics = self.__storage_engine.get_stats(database_name=self.__database, table_name=table)
         return statistics.V_a_r[attribute]
+    
+    # Time Cost ddengan waktu block transfer = waktu seeks = 1
+    
+    def linearScan(self, table: str):
+        statistics = self.__storage_engine.get_stats(database_name=self.__database, table_name=table)
+        return statistics.b_r + 1
+    
+    #Asumsi tinggi pohon diketahui
+    def indexScanKey(self, table: str, levelPohon): 
+        statistics = self.__storage_engine.get_stats(database_name=self.__database, table_name=table)
+        return 2*(levelPohon + 1)
+    
+    #Asumsi tinggi pohon diketahui
+    def indexScanNonkey(self, table: str, levelPohon): 
+        statistics = self.__storage_engine.get_stats(database_name=self.__database, table_name=table)
+        return 2*(levelPohon) + statistics.b_r + 1
+    
+    def nestedLoopJoin(self, table1: str, table2: str) :
+        statistics1 = self.__storage_engine.get_stats(database_name=self.__database, table_name=table1)
+        statistics2 = self.__storage_engine.get_stats(database_name=self.__database, table_name=table2)
+        return statistics1.n_r * statistics2.b_r +statistics1.b_r + statistics1.n_r +statistics1.b_r
+        
+    def blockNestedLoopJoin(self, table1: str, table2: str) :
+        statistics1 = self.__storage_engine.get_stats(database_name=self.__database, table_name=table1)
+        statistics2 = self.__storage_engine.get_stats(database_name=self.__database, table_name=table2)
+        return statistics1.b_r * statistics2.b_r + statistics1.b_r + 2*statistics1.b_r
+    
+    # Asumsi kedua tabel sudah di-sort dan block buffer berukuran 1
+    def mergeJoin(self, table1: str, table2: str) :
+        statistics1 = self.__storage_engine.get_stats(database_name=self.__database, table_name=table1)
+        statistics2 = self.__storage_engine.get_stats(database_name=self.__database, table_name=table2)
+        return 2*(statistics1.b_r * statistics2.b_r)
+    
+
