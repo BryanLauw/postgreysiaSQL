d0d4cd581eb73e6b6f7e46b38129cc1ca61ae772
bastianhs <140167233+bastianhs@users.noreply.github.com>
2024-11-22 15:47:31 +0700

    fix: query cost for where_comparison

--- QueryOptimizer/QueryCost.py
+++ QueryOptimizer/QueryCost.py
@@ -14,7 +14,7 @@ class QueryCost:
         statistics = self.__storage_engine.get_stats(database_name=self.__database, table_name=table)
         return statistics.n_r - self.where_equals(table=table, attribute=attribute)
     
-    def where_comparison(self, table: str, attribute: str) -> int:
+    def where_comparison(self, table: str) -> int:
         statistics = self.__storage_engine.get_stats(database_name=self.__database, table_name=table)
         # assume don't have min(A,r) and max(A,r)
         return statistics.n_r // 2
