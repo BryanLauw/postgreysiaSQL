192ad3473685c517ade9afdf7c541db144f91ba6
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-12-02 12:46:26 +0700

    Revert "feat: undo and or"
    This reverts commit db67792a5ff8b12618c431bfaa1e8e1c89085db9.

--- QueryOptimizer/OptimizationEngine.py
+++ QueryOptimizer/OptimizationEngine.py
@@ -132,6 +132,7 @@ if __name__ == "__main__":
     # cost_query = QueryCost(storage, "users")
     # print("COST = ", cost_query.get_cost(parsed_query.query_tree), "\n")
 
+
     try:
         invalid_query = "SELECT x.a FROM students AS s"
         print(invalid_query)
