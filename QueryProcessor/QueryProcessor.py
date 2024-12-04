304d7927e9b097c99d2404327435ea1fcf8cc21b
Ben <sihombingbenjamin2004@gmail.com>
2024-12-04 14:56:51 +0700

    fix: change joinOn to privat func

--- QueryProcessor/QueryProcessor.py
+++ QueryProcessor/QueryProcessor.py
@@ -276,7 +276,7 @@ class QueryProcessor:
         except Exception as e:
             return e
         
-    def joinOn(tablename1: str, tablename2:str, table1: List[map], table2: List[map], cond: str):
+    def __joinOn(self,tablename1: str, tablename2:str, table1: List[map], table2: List[map], cond: str):
         result = []
         condList = [['key1','key2'],['key3','key4']]
         for r1 in table1:
